53b8cdc36d987bb795b8e21ffe399556bfa664fc
EwuraAnnie <ewuranhyira03@gmail.com>
2021-12-23 08:48:48 +0000

    first commit

---
+++ lib/todo_home_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      scaffoldBackgroundColor: Color(0xffF0F4FC),
+     appBarTheme: AppBarTheme() 
+    )
+  ));
+}
